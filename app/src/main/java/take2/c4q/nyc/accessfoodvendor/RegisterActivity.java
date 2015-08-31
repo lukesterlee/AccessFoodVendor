3cada5efe1a0b5a7fa62282f14cec19918fdbdf9
Luke Desktop <lukesterlee@gmail.com>
2015-08-31 12:05:36 -0400

    start

---
+++ app/src/main/java/take2/c4q/nyc/accessfoodvendor/RegisterActivity.java
@@ -0,0 +1,10 @@
+package take2.c4q.nyc.accessfoodvendor;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Luke on 8/31/2015.
+ */
+public class RegisterActivity extends AppCompatActivity {
+
+}
